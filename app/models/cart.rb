16996d7f346c52484783fd802f8a144595c481c5
Ellen Junker <ellenjunker2000@gmail.com>
2025-08-19 19:08:14 -0300

    new model cart_item modifications cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,18 @@
 class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
-  # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
+  def total_price
+    cart_items.sum(&:total_price)
+  end
+
+  def inactive_for_3_hours?
+    updated_at < 3.hours.ago
+  end
+
+  def abandoned_for_7_days?
+    abandoned? && updated_at < 7.days.ago
+  end
 end
