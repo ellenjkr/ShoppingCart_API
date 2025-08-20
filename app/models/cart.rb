811613c68d7ec6ca1270b4800fe4c213d56f9ca4
Ellen Junker <ellenjunker2000@gmail.com>
2025-08-19 22:16:52 -0300

    use last_interaction_at change job file abandoned default false

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,10 +9,18 @@ class Cart < ApplicationRecord
   end
 
   def inactive_for_3_hours?
-    updated_at < 3.hours.ago
+    last_interaction_at.nil? || last_interaction_at < 1.minutes.ago
   end
 
   def abandoned_for_7_days?
-    abandoned? && updated_at < 7.days.ago
+    abandoned? && last_interaction_at < 2.minutes.ago
+  end
+
+  def mark_as_abandoned
+    update(abandoned: true) if inactive_for_3_hours?
+  end
+
+  def remove_if_abandoned
+    destroy if abandoned_for_7_days?
   end
 end
---
+++ db/migrate/20250819221104_add_abandoned_to_carts.rb
@@ -0,0 +1,5 @@
+class AddAbandonedToCarts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :carts, :abandoned, :boolean, default: false, null: false
+  end
+end
